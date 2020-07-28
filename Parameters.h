9feafa75be5267f320dad03bfd9425afb881935d
Alexey Dzyuba <a.dzyuba@gmail.com>
2020-07-28 13:25:15 +0000

    Add number of r_steps to constants

--- Parameters.h
+++ Parameters.h
@@ -1,5 +1,6 @@
 #include "TMath.h"
 const int N_ANODES = 9;
+const int N_R      = 500;
 const int N_TIME   = 200;
 double pi = TMath::Pi();
 
@@ -7,7 +8,7 @@ double pi = TMath::Pi();
 double grid_anode_distance = 10.    ; // mm     ; Grid-Anode distance
 double drift_velocity      = 0.0075 ; // mm/ns  ; Dirift velosity
 double step_size           = 0.1    ; // mm     ; step size for calculation of induced charge
-double channel_width       = 10     ; // ns     ; Flash ADC channel width
+double channel_width       = 40     ; // ns     ; Flash ADC channel width
 
 double r_map               = 50.    ; // mm  <--- To fix later!!!
 
@@ -20,8 +21,8 @@ double i_time[N_TIME];  // time-stamps for current map (in the center btw time-s
 
 // first index = map size
 // MUST FIT r_map/step_size
-double  charge[500][N_TIME];
-double current[500][N_TIME];
+double  charge[N_R][N_TIME];
+double current[N_R][N_TIME];
 
 double anode_area_of_overlap[N_ANODES];
 
